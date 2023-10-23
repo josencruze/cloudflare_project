569b62253d739c11fdaee5077450ba2f18fdde06
josencruze <102194461+josencruze@users.noreply.github.com>
2023-10-22 20:09:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -29,26 +29,18 @@ async function handleRequest(request) {
 }
 
 async function fetchUserInfo(request) {
-  // Implement the logic to fetch user information.
-  // You may need to use the Cloudflare API or some other mechanism to get user data.
-  // For example, you could fetch user data from your own database or an identity provider.
 
-  // Replace the following with your own implementation
-  // Here, we will simulate fetching user information from a database or external source.
   const userGoogleIdP = '142538743499-vuqncebr44c9lsb0qr7mkf5mdt2sudc3.apps.googleusercontent.com';
   const userEmail = 'josenirranjanc@gmail.com';
 
-  // Simulate fetching the user's country code (replace with actual logic to fetch it)
   let userCountry = await fetchCountryCode(request);
 
   // Check if the user is yourself (Google IdP SSO) or has an @cloudflare.com email address.
   if (userGoogleIdP === '142538743499-vuqncebr44c9lsb0qr7mkf5mdt2sudc3.apps.googleusercontent.com' || userEmail.endsWith('@cloudflare.com')) {
     // If the user is authenticated, set the country code.
     if (userGoogleIdP === '142538743499-vuqncebr44c9lsb0qr7mkf5mdt2sudc3.apps.googleusercontent.com') {
-      // Set the country code for yourself (replace with your actual country code)
       userCountry = 'canada';
     } else if (userEmail.endsWith('@cloudflare.com')) {
-      // Set the country code for @cloudflare.com users (replace with your actual country code)
       userCountry = 'cloudflare_country_code';
     }
 
@@ -67,11 +59,7 @@ async function fetchUserInfo(request) {
 }
 
 async function fetchCountryCode(request) {
-  // Implement the logic to fetch the user's country code.
-  // This can involve making a request to an external service or using other methods to determine the country code.
-  // Replace the following with your own implementation.
 
-  // Simulate fetching the country code (replace with actual logic)
   return 'user_country_code';
 }
 
